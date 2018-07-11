bcb051f7151b768e079657f5fa3de99f6ff06512
easonhan <nbkhic@qq.com>
2018-07-11 09:59:10 +0800

    add html test tool

---
+++ html_assertion.py
@@ -0,0 +1,43 @@
+from requests_html import HTMLSession
+from sys import argv
+DEBUG = True
+
+USAGE = '''
+USAGE:
+python html_assertion.py www.itest.info .thumbnail-img 4
+'''
+
+if len(argv) != 4:
+  print(USAGE)
+  exit(1)
+
+script_name, url, css_selector, length = argv 
+
+if url[:3] != 'http':
+  url = 'http://' + url
+
+session = HTMLSession()
+r = session.get(url)
+
+elements = r.html.find(css_selector)
+
+def debug():
+  if DEBUG: 
+    print('*' * 100)
+    print(f"css选择器: {css_selector}, 共找到{len(elements)}个元素\n")
+    for element in elements:
+      print(element.html)
+      print(element.attrs)
+      print()
+
+if len(elements) != int(length):
+  print(f"失败! 预期{length}个元素，实际存在{len(elements)}个元素\n")
+  debug()
+  exit(1)
+else:
+  print(f"成功!\n")
+  debug()
+
+
+
+
